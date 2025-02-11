31036ec313330b1da91d76d9b4e09457f4d266d5
cchatter123 <doty_om@psasm.biz>
2025-02-11 00:58:09 +0000

    new

---
+++ hello_world.py
@@ -0,0 +1,47 @@
+
+# FILE NAME: hello_world.py
+
+# NAME: Oliver Doty
+# DATE: 2/10/2025
+# BRIEF DESCRIPTION:  This program outputs "Hello, World!"
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. You can see the Sample Output below, too
+
+
+# Write your code below this line (add as many lines of code as you need)
+print("Hello, World!")
+
+
+
+
+
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Congratulations on your first lab! What is ONE thing you learned?
+how to print in python
+
+'''
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+Hello, World!
+
+'''
+
+
---
+++ rectangle.py
@@ -0,0 +1,60 @@
+# FILE NAME: rectangle.py
+
+# NAME: Oliver Doty
+# DATE: 2/10/2025
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+print("*****")
+print("*   *")
+print("*   *")
+print("*****")
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+
+*****
+*   *
+*   *
+*****
+
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. This is your first "real" lab. What is one thing that you struggled with?
+
+Nothing 
+
+
+
+'''
