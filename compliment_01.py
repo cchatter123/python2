3efb8ae9e65263033d7a637126aa033809fb2119
cchatter123 <doty_om@psasm.biz>
2025-03-04 03:19:43 +0000

    save

---
+++ compliment_01.py
@@ -0,0 +1,86 @@
+# FILE NAME - compliment_01.py
+
+# NAME: Oliver Doty
+# DATE: 3/3/2025
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+answer = input("Would you like a compliment")
+
+if answer == 'yes':
+    print"You have wonderful eyes.
+
+print("Thank you for playing.")
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Would you like a compliment? yes
+You have wonderful eyes.
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? y
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? Yes
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? no
+Thank you for playing.
+'''
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
+
+
+
+
+2. What was the hardest part of this lab?
+
+
+
+
+
+
+
+'''
---
+++ compliment_02.py
@@ -0,0 +1,80 @@
+# FILE NAME - compliment_02.py
+
+# NAME: Oliver Doty
+# DATE: 3/3/2025
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+########## ENTER YER CODE BELOW THIS LINE ##########
+
+
+
+
+
+
+
+
+
+
+########### END YER CODE ABOVE THIS LINE ###########
+
+    
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+Would you like a compliment? yes
+You have wonderful eyes.
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? Yes
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? y
+No compliment for you!
+Thank you for playing.
+'''
+
+
+'''
+Would you like a compliment? no
+No compliment for you!
+Thank you for playing.
+'''
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. Did you struggle with this lab (YES/NO)?
+
+
+
+
+
+
+
+'''
