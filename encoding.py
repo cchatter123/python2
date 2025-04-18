925e53cb9c756cece8c6d9e52b66ebd1de8ba379
cchatter123 <doty_om@psasm.biz>
2025-04-18 21:41:45 +0000

    save

--- encoding.py
+++ encoding.py
@@ -1,23 +1,15 @@
 import base64 
 
-# FILE NAME - decoding.py
+# FILE NAME - encoding.py
 
 # NAME - Oliver Doty
 # DATE - 4/18/2025
 # DESCRIPTION - 
 
-
-
-
+  
 file_path = input("File name? ")
 file = open(file_path, 'r')
 data = file.read()
-decoded_data = base64.b64decode(data)
-print(decoded_data)
+encoded_data = base64.b64encode(data.encode('utf-8'))
+print(encoded_data)
 file.close()
-# GET DATA
-
-    
-    
-    
-    # OUTPUT DECODED DATA
