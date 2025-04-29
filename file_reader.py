040c68b39b56ec7f68e6804f848862359c8b34d1
cchatter123 <doty_om@psasm.biz>
2025-04-29 18:31:05 +0000

    save

---
+++ file_reader.py
@@ -0,0 +1,103 @@
+# FILE NAME - file_reader.py
+
+# NAME: Oliver Doty
+# DATE: 4/29/2025
+# BRIEF DESCRIPTION:  
+
+
+
+# 1. Make sure you fill out the comments above
+# 2. Write your code in the proper spot
+# 3. Be sure to answer the Reflection Questions and Attestation below
+# 4. The Sample Output has been included in this code for your convenience
+
+
+
+
+
+
+########################################
+#          SAMPLE OUTPUT
+########################################
+
+'''
+File name? words01.txt
+
+Wolverine
+Rogue
+Psylocke
+Professor X
+Beast
+'''
+
+
+'''
+File name? words02.txt
+
+Lily
+Marshall
+Barney
+Robin
+Ted
+'''
+
+
+'''
+File name? words03.txt
+
+blubber
+macadamia
+gazebo
+spatula
+plethora
+ploy
+foible
+'''
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+
+1. What is an example of a time when you might want to output the contents
+   of a file?
+
+
+
+
+
+
+
+'''
+
+
+
+########################################
+#            ATTESTATION
+########################################
+
+'''
+
+Please gauge your utilization of AI on the following spectrum. Place an "X" in front
+of the appropriate response. Only choose one of the following:
+
+[ ] I did not use AI at all for this lab.
+[ ] I wrote the initial draft of the software but had AI help me make it better.
+[ ] I fed the lab description to AI and had it generate a response but I modified it.
+[ ] AI created the entire program for me.
+
+
+
+It is critical in this class that you understand the concepts as we explore them because
+those concepts are required understanding for entry level programming. Reliance on resources
+like AI and internet sites like Chegg, CourseHero, StackOverflow, and general Google results
+may impede your understanding. Please rate how well you understand the concepts in this lab: 
+
+[ ] I understand very little about this lab.
+[ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
+[ ] I pretty much get it.
+[ ] I'm solid. Totally got it.
+
+'''
